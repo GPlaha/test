d4c914580b4b1832829497da584bcd70a08f29d0
Gurprit Plaha <gurprit.plaha@softwareinstitute.com>
2022-03-24 17:16:54 +0000

    gameplay code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+int Feature1GamePlayCODE()
+{
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
+
 int feature1UICODE()
 {
     std::cout << "Hello World" << std::endl;
